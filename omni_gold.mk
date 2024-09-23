d9cfd3ea70efa4da96011c3572e13b77a0e02368
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-09-23 01:36:59 -0700

    Update omni_gold.mk

--- omni_gold.mk
+++ omni_gold.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from gold device
 $(call inherit-product, device/xiaomi/gold/device.mk)
 
 PRODUCT_DEVICE := gold
-PRODUCT_NAME := omni_gold
+PRODUCT_NAME := twrp_gold
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := gold
 PRODUCT_MANUFACTURER := xiaomi
